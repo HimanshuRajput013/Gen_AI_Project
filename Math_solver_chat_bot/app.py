8ee5856af84b4557dc90e3e4157c17dd3904ed8b
Himnanshu Rajput <hrajput0013@gmail.com>
2025-01-01 23:04:27 +0530

    first

---
+++ Math_solver_chat_bot/app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from langchain_groq import ChatGroq
+from langchain.chains import LLMMathChain, LLMChain
+from langchain.prompts import PromptTemplate
+from langchain_community.utilities import WikipediaAPIWrapper
+from langchain.agents.agent_types import AgentType
+from langchain.agents import Tool, initialize_agent
+from langchain.callbacks import StreamlitCallbackHandler
+
+## Set upi the Stramlit app
+st.set_page_config(page_title="Text To MAth Problem Solver And Data Serach Assistant",page_icon="🧮")
+st.title("Text To Math Problem Solver Uing Google Gemma 2")
+
+groq_api_key=st.sidebar.text_input(label="Groq API Key",type="password")
+
+
+
+
+
+
+
+
+
+
+
